6db29f6e64527ea046cd43169e219793c9ed765a
Daniel <shontandaniel@gmail.com>
2025-08-30 21:18:05 +0100

    Update next js build

--- meditrust-ng-web/store/useUIStore.ts
+++ meditrust-ng-web/store/useUIStore.ts
@@ -1,12 +1,16 @@
 'use client'
 import { create } from 'zustand'
-export const useUIStore = create(set => ({
-  triageResult: null,
-  selectedDoctorId: null,
-  selectedSlot: null,
+
+interface UIState {
+  triageResult: any 
+  showDesktopPrompt: boolean
+  setTriageResult: (value: any ) => void
+  setShowDesktopPrompt: (value: boolean) => void
+}
+
+export const useUIStore = create<UIState>(set => ({
+  triageResult:  null,
   showDesktopPrompt: false,
   setTriageResult: (r) => set({ triageResult: r }),
-  setSelectedDoctor: (id) => set({ selectedDoctorId: id }),
-  setSelectedSlot: (s) => set({ selectedSlot: s }),
   setShowDesktopPrompt: (v) => set({ showDesktopPrompt: v })
 }))
